c5fe85ad3ba164a86e81788df7a64a504977cc83
s4553711 <s4553711@gmail.com>
2017-02-03 11:17:54 +0800

    output log to file while the scriptFile variable is provided

--- src/common.sh
+++ src/common.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 log() {
-	echo "[$(date +"%Y-%m-%d %H:%M:%S") "`hostname`"] $(basename ${BASH_SOURCE[0]}|sed 's/\.sh//g'), INFO: $1"
+	out=$(echo $scriptFile|sed 's/\.sh/\.log/g')
+	if [ -z $out ]; then
+		echo "[$(date +"%Y-%m-%d %H:%M:%S") "`hostname`"] $(basename ${BASH_SOURCE[0]}|sed 's/\.sh//g'), INFO: $1"
+	else
+		echo "[$(date +"%Y-%m-%d %H:%M:%S") "`hostname`"] $(basename $scriptFile), INFO: $1" >> $out
+	fi
 }
 
 error() {
-	echo "[$(date +"%Y-%m-%d %H:%M:%S") "`hostname`"] $(basename ${BASH_SOURCE[0]}|sed 's/\.sh//g'), ERROR: $1"
+	out=$(echo $scriptFile|sed 's/\.sh/\.log/g')
+	if [ -z $out ]; then
+		echo "[$(date +"%Y-%m-%d %H:%M:%S") "`hostname`"] $(basename ${BASH_SOURCE[0]}|sed 's/\.sh//g'), ERROR: $1"
+	else
+		echo "[$(date +"%Y-%m-%d %H:%M:%S") "`hostname`"] $(basename $scriptFile), ERROR: $1" >> $out
+	fi
 }
 
 join_by() { local IFS="$1"; shift; echo "$*"; }
 
 exec() {
 	cmd=$1
-	set -x
+	#set -x
 	eval "$cmd"
 					    
 	if [ $? -ne 0  ]; then
